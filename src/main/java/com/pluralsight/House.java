5eafa82dec96e01621fd8afdb7a3e98aea4e0db4
yuujn <pale.cup5223@fastmail.com>
2026-05-12 11:52:10 -0500

    implement House class

---
+++ src/main/java/com/pluralsight/House.java
@@ -0,0 +1,59 @@
+package com.pluralsight;
+
+public class House extends Asset {
+    private String address;
+    private int condition;
+    private int squareFoot;
+    private int lotSize;
+
+    public House(String description, String dateAcquired, double originalCost, String address, int condition, int squareFoot, int lotSize) {
+        super(description, dateAcquired, originalCost);
+        this.address = address;
+        this.condition = condition;
+        this.squareFoot = squareFoot;
+        this.lotSize = lotSize;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCondition() {
+        return condition;
+    }
+
+    public void setCondition(int condition) {
+        this.condition = condition;
+    }
+
+    public int getSquareFoot() {
+        return squareFoot;
+    }
+
+    public void setSquareFoot(int squareFoot) {
+        this.squareFoot = squareFoot;
+    }
+
+    public int getLotSize() {
+        return lotSize;
+    }
+
+    public void setLotSize(int lotSize) {
+        this.lotSize = lotSize;
+    }
+
+    @Override
+    public double getOriginalCost() {
+        return (.25 * lotSize) + switch (condition) {
+            case 1 -> 180. * squareFoot;
+            case 2 -> 130. * squareFoot;
+            case 3 -> 90. * squareFoot;
+            case 4 -> 80. * squareFoot;
+            default -> throw new RuntimeException("Invalid condition specified");
+        };
+    }
+}
