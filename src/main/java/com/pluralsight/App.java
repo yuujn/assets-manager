c5284310c9813e36e32fd4f9e385db27f7422fff
yuujn <pale.cup5223@fastmail.com>
2026-05-12 13:13:14 -0500

    print a list of example assets

---
+++ src/main/java/com/pluralsight/App.java
@@ -0,0 +1,55 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+
+public class App {
+    public static void main(String[] args) {
+        ArrayList<Asset> assets = new ArrayList<>();
+        assets.add(new House(
+                "John's Summer House",
+                "April 27th, 2022",
+                500_000.,
+                "42 Universe Way",
+                1,
+                1000,
+                10000
+        ));
+        assets.add(new House(
+                "John's Regular House",
+                "April 27th, 2042",
+                1_000_000.,
+                "43 Universe Way",
+                1,
+                10000,
+                50000
+        ));
+        assets.add(new Vehicle(
+                "John's Definitely Real Car",
+                "April 27th, 2015",
+                50_000.,
+                "Toyota Avalon",
+                2014,
+                10000
+        ));
+        assets.add(new Vehicle(
+                "John's Definitely Real Second Car",
+                "April 27th, 2016",
+                60_000.,
+                "Hyundai Elantra",
+                2016,
+                10000
+        ));
+
+        boolean isFirst = true;
+        for (Asset asset : assets) {
+            if (!isFirst) {
+                System.out.println("---------------------------");
+            }
+            isFirst = false;
+            System.out.printf("Description:      %s%n", asset.getDescription());
+            System.out.printf("Acquisition Date: %s%n", asset.getDateAcquired());
+            System.out.printf("Original Cost:    %s%n", asset.getOriginalCost());
+            System.out.printf("Value:            %s%n", asset.getValue());
+        }
+    }
+}
